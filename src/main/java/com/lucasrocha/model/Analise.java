fd05173c7ea0d59c7e054d1e9d5a1da7bc8a7509
Lucas Rocha <49926164+futuruz-rt@users.noreply.github.com>
2021-03-02 17:46:40 -0300

    Create Analise.java
    Abstração da classe Análise, a partir da Planilha. Foram implementadas as possíveis variáveis a serem utilizadas.

---
+++ src/main/java/com/lucasrocha/model/Analise.java
@@ -0,0 +1,21 @@
+package com.lucasrocha.model;
+
+import java.util.Date;
+
+public class Analise {
+  //informações que foram preenchidas no exemplo: talhão (inteiro), área do talhão (ha), área total (ha), profundidade (cm), textura do solo (índices)
+    private Integer numeroTalhao;
+    private Double areaTalhao;
+    private Double areaTotal;
+    private Integer texturaSolo;
+    private String respTecnico;
+    private Double profundidadeAmostraSoloCm;
+    private Double resultadoAnalise;
+  
+    /* essas informações não foram preenchidas, mas podem ser útil
+    private Date dataAnalise;
+    private String municipio;
+    private String lote;
+    private Integer matriculaLote;
+    */
+}
