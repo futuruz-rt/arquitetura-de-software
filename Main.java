008ee7012879393a7f29da0f517533d60e6d5a3f
Lucas Rocha <49926164+futuruz-rt@users.noreply.github.com>
2021-03-02 11:54:51 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+ class Main {
+
+    /**
+     * Programa que será incrementado para construir os dados da planilha
+     
+     */
+    public static void main(String[] args)
+    {
+        //1 - identificar as variáveis que serão necessária no cálculo
+        
+        
+    } //fim do metodo main
+}
