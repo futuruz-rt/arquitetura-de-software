7e13732b929ca228ba65316a908e9e8d454c535e
Lucas Rocha <49926164+futuruz-rt@users.noreply.github.com>
2021-03-03 21:42:43 -0300

    Exemplo de comparação
    O próximo passo é criar a lógica das contas contidas na planilha do excel e utilizar nos atributos da classe Analise.java

--- src/main/java/com/lucasrocha/App.java
+++ src/main/java/com/lucasrocha/App.java
@@ -11,27 +11,38 @@ public class App
 	//leitura dos elementos
 	Scanner teclado = new Scanner(System.in);		
 	System.out.println("Informe a quantidade para os elementos da analise:");	
-		
+	    
+	System.out.println("\nInforme a textetura do solo (1 - Argiloso; 2 - Textura média):");
+	elementos.setTexturaSolo(teclado.nextInt());	   
+	    
 	System.out.println("\nQuantidade de fósforo (mg.dm3 | mehlich): ");
-	elementos.getElementosAtuais().setFosforo(teclado.nextDouble());
+	elementos.setFosforo(teclado.nextDouble());
        
 	System.out.println("\nQuantidade de potássio (cmol): ");
-	elementos.getElementosAtuais().setPotassio(teclado.nextDouble());
+	elementos.setPotassio(teclado.nextDouble());
        
 	System.out.println("\nQuantidade de cálcio (cmol): ");
-	elementos.getElementosAtuais().setCalcio(teclado.nextDouble());
+	elementos.setCalcio(teclado.nextDouble());
        
 	System.out.println("\nQuantidade de magnésio (cmol): ");
-	elementos.getElementosAtuais().setMagnesio(teclado.nextDouble());
+	elementos.setMagnesio(teclado.nextDouble());
        
 	System.out.println("\nQuantidade de Enxofre (mg.dm³): ");
-	elementos.getElementosAtuais().setEnxofre(teclado.nextDouble());
+	elementos.setEnxofre(teclado.nextDouble());
        
 	System.out.println("\nQuantidade de aluminio: ");
-	elementos.getElementosAtuais().setAluminio(teclado.nextDouble());
+	elementos.setAluminio(teclado.nextDouble());
 		
 	System.out.println("\nQuantidade de H+AL: ");
-	elementos.getElementosAtuais().sethAl(teclado.nextDouble());	    
-
+	elementos.sethAl(teclado.nextDouble());	    
+	
+	//COMPARAÇÃO DOS DADOS INFORMADOS COM OS DADOS IDEAIS
+	System.out.println("Teste de Exemplo");
+	System.out.println("\nFosforo atual: " + elementos.getFosforo() + " Fosforo ideal: " + elementos.getFosforoIdeal());
+	System.out.println("\nPotassio atual:: " + elementos.getPotassio() + " Potassio ideal: " + elementos.getPotassioIdeal());
+	System.out.println("\nCalcio atual: " + elementos.getCalcio() + " Calcio ideal: " + elementos.getCalcioIdeal());
+	System.out.println("\nMagnesio atual: " + elementos.getMagnesio() + " Potassio ideal: " + elementos.getMagnesioIdeal());
+	System.out.println("\nEnxofre atual: " + elementos.getEnxofre() + " Enxofre ideal: " + elementos.getEnxofreIdeal());
+	System.out.println("\nAluminio atual: " + elementos.getAluminio() + " Aluminio ideal: " + elementos.getAluminioIdeal());
     }
 }
