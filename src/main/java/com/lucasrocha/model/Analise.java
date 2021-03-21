a1731f328dbafb6225ab70cf78963ff27311d2b8
Lucas Rocha <49926164+futuruz-rt@users.noreply.github.com>
2021-03-21 18:05:55 -0300

    Atualização classe Analise.java

--- src/main/java/com/lucasrocha/model/Analise.java
+++ src/main/java/com/lucasrocha/model/Analise.java
@@ -1,106 +1,109 @@
-package com.lucasrocha.model;
-import model.Elementos;
-import java.util.Date;
+package com.lucasrocha.arquitetura.model;
 
 public class Analise {
     //informações que foram preenchidas no exemplo: talhão (inteiro), área do talhão (ha), área total (ha), profundidade (cm)
     // dados que serão utilizados em cálculos
-    private int numeroTalhao;
+
+    private int ntalhao;
     private double areaTalhao;
     private double areaTotal;
-    private int sistemaCultivo;    
+    private int sistemaCultivo;
     private double profundidadeAmostraSoloCm;
-    private double resultadoAnalise;    
+    private double resultadoAnalise;
     //controle dos dados
     private String produtor;
-    private Date dataAnalise;
+    private String data;
     private String municipio;
     private String lote;
     private String matriculaLote;
-    private String respTecnico;    
-       
+    private String respTecnico;
+
     //métodos SET e GET  
     public void setTalhao(int talhao) {
-      this.talhao = talhao;
+        this.ntalhao = talhao;
     }
-  
+
     public int getTalhao() {
-        return talhao;
-      }
-  
+        return ntalhao;
+    }
+
     public void setAreaTalhao(double areaTalhao) {
-      this.areaTalhao = areaTalhao;
+        this.areaTalhao = areaTalhao;
     }
-  
+
     public double getAreaTalhao() {
-      return areaTalhao;
+        return areaTalhao;
     }
-  
+
     public void setAreaTotal(double areaTotal) {
-      this.areaTotal = areaTotal;
+        this.areaTotal = areaTotal;
     }
 
     public double getAreaTotal() {
-      return areaTotal;
-    }  
-    
+        return areaTotal;
+    }
+
     public void setSistemaCultivo(int sistemaCultivo) {
-      this.sistemaCultivo = sistemaCultivo;
+        this.sistemaCultivo = sistemaCultivo;
     }
-    
+
     public int getSistemaCultivo() {
-      return sistemaCultivo;
+        return sistemaCultivo;
     }
-    
-    public void setProfundidadeAmostraCm(double profundidadeAmostra) {
-      this.profundidadeAmostra = profundidadeAmostra;
+
+    public void setProfundidadeAmostraCm(double profundidadeAmostraCm) {
+        this.profundidadeAmostraSoloCm = profundidadeAmostraCm;
     }
 
     public double getProfundidadeAmostraCm() {
-      return profundidadeAmostra;
+        return profundidadeAmostraSoloCm;
     }
-    
+
     public void setResultadoAnalise(int resultadoAnalise) {
-      this.resultadoAnalise = resultadoAnalise;
+        this.resultadoAnalise = resultadoAnalise;
     }
-  
-    public int getResultadoAnalise() {
-      return resultadoAnalise;
-    }   
-     
+
+    public double getResultadoAnalise() {
+        return resultadoAnalise;
+    }
+
     public String getProdutor() {
-      return produtor;
+        return produtor;
     }
+
     public void setProdutor(String produtor) {
-      this.produtor = produtor;
+        this.produtor = produtor;
     }
 
     public String getData() {
-      return data;
+        return data;
     }
+
     public void setData(String data) {
-      this.data = data;
+        this.data = data;
     }
 
     public String getMunicipio() {
-      return municipio;
+        return municipio;
     }
+
     public void setMunicipio(String municipio) {
-      this.municipio = municipio;
+        this.municipio = municipio;
     }
-    
+
     public void setMatriculaLote(String matriculaLote) {
-      this.matriculaLote = matriculaLote;
+        this.matriculaLote = matriculaLote;
     }
-  
+
     public String getMatriculaLote() {
-      return matriculaLote;
+        return matriculaLote;
     }
-  
-     public void setRespTecnico(String respTecnico) {
-      this.respTecnico = respTecnico;
+
+    public void setRespTecnico(String respTecnico) {
+        this.respTecnico = respTecnico;
     }
-    
+
     public String getRespTecnico() {
-      return respTecnico;
+        return respTecnico;
     }
+}
