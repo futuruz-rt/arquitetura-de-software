f8e223a5663d07018adfc1a41c2679a94c84d6f2
Lucas Rocha <49926164+futuruz-rt@users.noreply.github.com>
2021-04-02 23:42:08 -0300

    Valores double atribuídos na classe enum

--- src/main/java/com/lucasrocha/model/FontesFosforo.java
+++ src/main/java/com/lucasrocha/model/FontesFosforo.java
@@ -1,16 +1,28 @@
 package com.lucasrocha.arquitetura.model;
 
 public enum FontesFosforo {
-    SUPERFOSFATO_SIMPLES,
-    SUPERFOSFATO_TRIPLO,
-    MAP,
-    DAP,
-    TERMOFOSTATO_YOORIN,
-    FOSFATO_ARAD,
-    FOSFATO_GAFSA,
-    FOSFATO_DAOUI,
-    FOSFATO_NAT_PATOS_MINAS,
-    ESCORIA_THOMAS,
-    ACIDO_FOSFORICO,
-    MULTIFOSFATO_MAGNESIANO
+    SUPERFOSFATO_SIMPLES(18.0),
+    SUPERFOSFATO_TRIPLO(41.0),
+    MAP(48.0),
+    DAP(45.0),
+    TERMOFOSTATO_YOORIN(18.0),
+    FOSFATO_ARAD(33.0),
+    FOSFATO_GAFSA(29.0),
+    FOSFATO_DAOUI(32.0),
+    FOSFATO_NAT_PATOS_MINAS(24.0),
+    ESCORIA_THOMAS(18.5),
+    ACIDO_FOSFORICO(52.0),
+    MULTIFOSFATO_MAGNESIANO(18.0);
+
+    private double valorFosforo;
+
+    FontesFosforo(double valorFosforo) {
+        this.valorFosforo = valorFosforo;
+    } 
+
+    public double getValorFosforo() {
+        return valorFosforo;
+    }
+    
+    
 }
