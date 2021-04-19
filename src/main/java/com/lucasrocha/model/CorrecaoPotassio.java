04f03b66b4c0295857b6855a0629848f506069a3
Lucas Rocha <49926164+futuruz-rt@users.noreply.github.com>
2021-04-19 20:05:10 -0300

    Remoção de um switch inteiro e variável auxiliar

--- src/main/java/com/lucasrocha/model/CorrecaoPotassio.java
+++ src/main/java/com/lucasrocha/model/CorrecaoPotassio.java
@@ -6,7 +6,7 @@ public class CorrecaoPotassio {
     private EquilibrioCorrecaoCTC ecc;
     private FontesPotassio fontePotassio;
     private double valorFontePotassio;
-
+    
     public CorrecaoPotassio(double participacaoPotassio, EquilibrioCorrecaoCTC ecc, FontesPotassio fontePotassio, double valorFontePotassio) {
         this.participacaoPotassio = participacaoPotassio;
         this.ecc = ecc;
@@ -33,19 +33,6 @@ public class CorrecaoPotassio {
         }
     }
 
-    public double valorFontePotassio() {
-        switch (this.fontePotassio) {
-            case CLORETO_POTASSIO:
-                return 58.0;
-            case SULFATO_POTASSIO:
-                return 52.0;
-            case SULFATO_POTASSIO_MAGNESIO:
-                return 22.0;
-            default:
-                return 0.0;
-        }
-    }
-
     public double quantidadeAplicarPotassio() {
         double necessidadeAdicionar = this.ecc.getPotassio()
                 * this.participacaoPotassio / this.participacaoPotassioAtual()
@@ -53,34 +40,27 @@ public class CorrecaoPotassio {
         if (necessidadeAdicionar < 0.01) {
             return 0.0;
         } else {
-            return necessidadeAdicionar * 39.1 * 10 * 2 * 1.2 * 100 / 0.85 / 100 * 100
-                    / this.valorFontePotassio();
+            return necessidadeAdicionar * 39.1 * 10 * 2 * 1.2 * 100 / 0.85
+                    / fontePotassio.getValorPotassio();
         }
     }
 
     public ForneceCorrecao forneceCorrecaoPotassioUm() {
-        double itemValor = 0;
-        switch (this.fontePotassio) {
-            case SULFATO_POTASSIO:
-                itemValor = this.quantidadeAplicarPotassio() * 0.17;
-                return new ForneceCorrecao(itemValor, Nutrientes.ENXOFRE);
-            case SULFATO_POTASSIO_MAGNESIO:
-                itemValor = this.quantidadeAplicarPotassio() * 0.22;
-                return new ForneceCorrecao(itemValor, Nutrientes.ENXOFRE);
-            default:
-                return null;
+        if (this.fontePotassio == FontesPotassio.SULFATO_POTASSIO) {
+            return new ForneceCorrecao(this.quantidadeAplicarPotassio() * 0.17, Nutrientes.ENXOFRE);
         }
+        if (this.fontePotassio == FontesPotassio.SULFATO_POTASSIO_MAGNESIO) {
+            return new ForneceCorrecao(this.quantidadeAplicarPotassio() * 0.22, Nutrientes.ENXOFRE);
+        }
+        return null;
+
     }
 
     public ForneceCorrecao forneceCorrecaoPotassioDois() {
-        double itemValor;
-        switch (this.fontePotassio) {
-            case SULFATO_POTASSIO_MAGNESIO:
-                itemValor = this.quantidadeAplicarPotassio() * 0.18;
-                return new ForneceCorrecao(itemValor, Nutrientes.MAGNESIO);
-            default:
-                return null;
+        if (this.fontePotassio == FontesPotassio.SULFATO_POTASSIO_MAGNESIO) {
+            return new ForneceCorrecao(this.quantidadeAplicarPotassio() * 0.18, Nutrientes.MAGNESIO);
         }
+        return null;
     }
 
     public double custoAlqueirePotassio() {
