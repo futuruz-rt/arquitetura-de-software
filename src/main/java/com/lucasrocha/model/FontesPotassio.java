a4d40c31d7a199f19b9e90919fec7e82775c5764
Lucas Rocha <49926164+futuruz-rt@users.noreply.github.com>
2021-04-19 20:03:52 -0300

    Valores atribuídos direto no enum

--- src/main/java/com/lucasrocha/model/FontesPotassio.java
+++ src/main/java/com/lucasrocha/model/FontesPotassio.java
@@ -1,7 +1,17 @@
 package com.lucasrocha.arquitetura.model;
 
 public enum FontesPotassio {
-    CLORETO_POTASSIO,
-    SULFATO_POTASSIO,
-    SULFATO_POTASSIO_MAGNESIO
+    CLORETO_POTASSIO(58.0),
+    SULFATO_POTASSIO(52.0),
+    SULFATO_POTASSIO_MAGNESIO(22.0);
+
+    public double valorFontePotassio;
+
+    FontesPotassio(double valorFontePotassio) {
+        this.valorFontePotassio = valorFontePotassio;
+    }
+
+    public double getValorPotassio() {
+        return valorFontePotassio;
+    }
 }
