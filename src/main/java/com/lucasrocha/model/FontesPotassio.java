53f9dddbb6dcde71f25c6034cb97d28291b6c264
Lucas Rocha <49926164+futuruz-rt@users.noreply.github.com>
2021-04-25 20:38:30 -0300

    Uso de Strategy para eliminação de condicionais (enum)

--- src/main/java/com/lucasrocha/model/FontesPotassio.java
+++ src/main/java/com/lucasrocha/model/FontesPotassio.java
@@ -1,10 +1,25 @@
 package com.lucasrocha.arquitetura.model;
 
 public enum FontesPotassio {
+    
     CLORETO_POTASSIO(58.0),
-    SULFATO_POTASSIO(52.0),
-    SULFATO_POTASSIO_MAGNESIO(22.0);
-
+    SULFATO_POTASSIO(52.0) {
+        @Override
+        public ForneceCorrecao getCorrecaoPotassioUm(double quantidadeAplicacao) {
+            return new ForneceCorrecao(quantidadeAplicacao * 0.17, Nutrientes.ENXOFRE);  
+        };
+    },
+    SULFATO_POTASSIO_MAGNESIO(22.0) {
+        @Override
+        public ForneceCorrecao getCorrecaoPotassioUm(double quantidadeAplicacao) {
+            return new ForneceCorrecao(quantidadeAplicacao * 0.22, Nutrientes.ENXOFRE); 
+        };
+        @Override
+        public ForneceCorrecao getCorrecaoPotassioDois(double quantidadeAplicacao) {
+            return new ForneceCorrecao(quantidadeAplicacao * 0.18, Nutrientes.MAGNESIO);  
+        }
+    };   
+    
     public double valorFontePotassio;
 
     FontesPotassio(double valorFontePotassio) {
@@ -13,5 +28,13 @@ public enum FontesPotassio {
 
     public double getValorPotassio() {
         return valorFontePotassio;
+    }
+    
+     public ForneceCorrecao getCorrecaoPotassioUm(double quantidadeAplicacao) {
+        return null;
+    }
+    
+     public ForneceCorrecao getCorrecaoPotassioDois(double quantidadeAplicacao) {
+        return null;
     }
 }
