574c6b1f3c815cbd201d670bf58014297250cf2b
Lucas Rocha <49926164+futuruz-rt@users.noreply.github.com>
2021-04-25 21:25:20 -0300

    Atualização dos testes

--- src/test/java/CorrecaoFosforoTest.java
+++ src/test/java/CorrecaoFosforoTest.java
@@ -1,4 +1,3 @@
-
 import com.lucasrocha.arquitetura.model.CorrecaoFosforo;
 import com.lucasrocha.arquitetura.model.FontesFosforo;
 import com.lucasrocha.arquitetura.model.EquilibrioCorrecaoCTC;
@@ -11,127 +10,64 @@ import org.junit.jupiter.api.Test;
 public class CorrecaoFosforoTest {
 
     @Test
-    public void testQuantidadeFosforoAplicacao() {
+    public void testquantidadeFosforoAplicar() {
         EquilibrioCorrecaoCTC ecc = new EquilibrioCorrecaoCTC(TexturaSolo.ARGILOSO, 8.59, 0.15, 5.76, 1.63, 3.67, 0.0, 0.0, 30.7);
         CorrecaoFosforo correcaoFosforo = new CorrecaoFosforo(12, FontesFosforo.SUPERFOSFATO_SIMPLES, 70, 1260, ecc);
-        assertEquals(123.95, correcaoFosforo.quantidadeFosforoAplicacao(), 0.1);
+        assertEquals(123.95, correcaoFosforo.quantidadeFosforoAplicar(), 0.1);
 
         correcaoFosforo = new CorrecaoFosforo(12, FontesFosforo.SUPERFOSFATO_TRIPLO, 70, 1260, ecc);
-        assertEquals(54.41, correcaoFosforo.quantidadeFosforoAplicacao(), 0.1);
+        assertEquals(54.41, correcaoFosforo.quantidadeFosforoAplicar(), 0.1);
 
         correcaoFosforo = new CorrecaoFosforo(12, FontesFosforo.MAP, 70, 1260, ecc);
-        assertEquals(46.48, correcaoFosforo.quantidadeFosforoAplicacao(), 0.1);
+        assertEquals(46.48, correcaoFosforo.quantidadeFosforoAplicar(), 0.1);
 
         correcaoFosforo = new CorrecaoFosforo(12, FontesFosforo.DAP, 70, 1260, ecc);
-        assertEquals(49.58, correcaoFosforo.quantidadeFosforoAplicacao(), 0.1);
+        assertEquals(49.58, correcaoFosforo.quantidadeFosforoAplicar(), 0.1);
 
         correcaoFosforo = new CorrecaoFosforo(12, FontesFosforo.TERMOFOSTATO_YOORIN, 70, 1260, ecc);
-        assertEquals(123.95, correcaoFosforo.quantidadeFosforoAplicacao(), 0.1);
+        assertEquals(123.95, correcaoFosforo.quantidadeFosforoAplicar(), 0.1);
 
         correcaoFosforo = new CorrecaoFosforo(12, FontesFosforo.FOSFATO_ARAD, 70, 1260, ecc);
-        assertEquals(67.60, correcaoFosforo.quantidadeFosforoAplicacao(), 0.1);
+        assertEquals(67.60, correcaoFosforo.quantidadeFosforoAplicar(), 0.1);
 
         correcaoFosforo = new CorrecaoFosforo(12, FontesFosforo.FOSFATO_GAFSA, 70, 1260, ecc);
-        assertEquals(76.93, correcaoFosforo.quantidadeFosforoAplicacao(), 0.1);
+        assertEquals(76.93, correcaoFosforo.quantidadeFosforoAplicar(), 0.1);
 
         correcaoFosforo = new CorrecaoFosforo(12, FontesFosforo.FOSFATO_DAOUI, 70, 1260, ecc);
-        assertEquals(69.72, correcaoFosforo.quantidadeFosforoAplicacao(), 0.1);
+        assertEquals(69.72, correcaoFosforo.quantidadeFosforoAplicar(), 0.1);
 
         correcaoFosforo = new CorrecaoFosforo(12, FontesFosforo.FOSFATO_NAT_PATOS_MINAS, 70, 1260, ecc);
-        assertEquals(92.96, correcaoFosforo.quantidadeFosforoAplicacao(), 0.1);
+        assertEquals(92.96, correcaoFosforo.quantidadeFosforoAplicar(), 0.1);
 
         correcaoFosforo = new CorrecaoFosforo(12, FontesFosforo.ESCORIA_THOMAS, 70, 1260, ecc);
-        assertEquals(120.60, correcaoFosforo.quantidadeFosforoAplicacao(), 0.1);
+        assertEquals(120.60, correcaoFosforo.quantidadeFosforoAplicar(), 0.1);
 
         correcaoFosforo = new CorrecaoFosforo(12, FontesFosforo.ACIDO_FOSFORICO, 70, 1260, ecc);
-        assertEquals(42.90, correcaoFosforo.quantidadeFosforoAplicacao(), 0.1);
+        assertEquals(42.90, correcaoFosforo.quantidadeFosforoAplicar(), 0.1);
 
         correcaoFosforo = new CorrecaoFosforo(12, FontesFosforo.MULTIFOSFATO_MAGNESIANO, 70, 1260, ecc);
-        assertEquals(123.95, correcaoFosforo.quantidadeFosforoAplicacao(), 0.1);
+        assertEquals(123.95, correcaoFosforo.quantidadeFosforoAplicar(), 0.1);
     }
 
     @Test
-    public void testForneceCorrecaoFosforoUm() {
+    public void testForneceCorrecaoFosforo() {
         EquilibrioCorrecaoCTC ecc = new EquilibrioCorrecaoCTC(TexturaSolo.ARGILOSO, 8.59, 0.15, 5.76, 1.63, 3.67, 0.0, 0.0, 30.7);
         CorrecaoFosforo correcaoFosforo = new CorrecaoFosforo(12, FontesFosforo.SUPERFOSFATO_SIMPLES, 70, 1260, ecc);
         ForneceCorrecao correcao = new ForneceCorrecao(12.39, Nutrientes.ENXOFRE);
-        assertEquals(correcao.getValor(), correcaoFosforo.forneceCorrecaoFosforoUm().getValor(), 0.1);
-        assertEquals(correcao.getDescricao_item(), correcaoFosforo.forneceCorrecaoFosforoUm().getDescricao_item());
+        assertEquals(correcao.getValor(), correcaoFosforo.forneceCorrecaoFosforo().getValor(), 0.1);
+        assertEquals(correcao.getDescricao_item(), correcaoFosforo.forneceCorrecaoFosforo().getDescricao_item());
 
         correcaoFosforo = new CorrecaoFosforo(12, FontesFosforo.TERMOFOSTATO_YOORIN, 70, 1260, ecc);
         correcao = new ForneceCorrecao(18.59, Nutrientes.MAGNESIO);
-        assertEquals(correcao.getValor(), correcaoFosforo.forneceCorrecaoFosforoUm().getValor(), 0.1);
-        assertEquals(correcao.getDescricao_item(), correcaoFosforo.forneceCorrecaoFosforoUm().getDescricao_item());
+        assertEquals(correcao.getValor(), correcaoFosforo.forneceCorrecaoFosforo().getValor(), 0.1);
+        assertEquals(correcao.getDescricao_item(), correcaoFosforo.forneceCorrecaoFosforo().getDescricao_item());
 
         correcaoFosforo = new CorrecaoFosforo(12, FontesFosforo.MULTIFOSFATO_MAGNESIANO, 70, 1260, ecc);
         correcao = new ForneceCorrecao(13.63, Nutrientes.ENXOFRE);
-        assertEquals(correcao.getValor(), correcaoFosforo.forneceCorrecaoFosforoUm().getValor(), 0.1);
-        assertEquals(correcao.getDescricao_item(), correcaoFosforo.forneceCorrecaoFosforoUm().getDescricao_item());
+        assertEquals(correcao.getValor(), correcaoFosforo.forneceCorrecaoFosforo().getValor(), 0.1);
+        assertEquals(correcao.getDescricao_item(), correcaoFosforo.forneceCorrecaoFosforo().getDescricao_item());        
     }
 
-    @Test
-    public void testForneceCorrecaoFosforoDois() {
-        EquilibrioCorrecaoCTC ecc = new EquilibrioCorrecaoCTC(TexturaSolo.ARGILOSO, 8.59, 0.15, 5.76, 1.63, 3.67, 0.0, 0.0, 30.7);
-        CorrecaoFosforo correcaoFosforo = new CorrecaoFosforo(12, FontesFosforo.SUPERFOSFATO_SIMPLES, 70, 1260, ecc);
-        ForneceCorrecao correcao = new ForneceCorrecao(34.70, Nutrientes.CALCIO);
-        assertEquals(correcao.getValor(), correcaoFosforo.forneceCorrecaoFosforoDois().getValor(), 0.1);
-        assertEquals(correcao.getDescricao_item(), correcaoFosforo.forneceCorrecaoFosforoDois().getDescricao_item());
-
-        correcaoFosforo = new CorrecaoFosforo(12, FontesFosforo.SUPERFOSFATO_TRIPLO, 70, 1260, ecc);
-        correcao = new ForneceCorrecao(10.88, Nutrientes.CALCIO);
-        assertEquals(correcao.getValor(), correcaoFosforo.forneceCorrecaoFosforoDois().getValor(), 0.1);
-        assertEquals(correcao.getDescricao_item(), correcaoFosforo.forneceCorrecaoFosforoDois().getDescricao_item());
-
-        correcaoFosforo = new CorrecaoFosforo(12, FontesFosforo.MAP, 70, 1260, ecc);
-        correcao = new ForneceCorrecao(4.18, Nutrientes.NITROGENIO);
-        assertEquals(correcao.getValor(), correcaoFosforo.forneceCorrecaoFosforoDois().getValor(), 0.1);
-        assertEquals(correcao.getDescricao_item(), correcaoFosforo.forneceCorrecaoFosforoDois().getDescricao_item());
-
-        correcaoFosforo = new CorrecaoFosforo(12, FontesFosforo.DAP, 70, 1260, ecc);
-        correcao = new ForneceCorrecao(7.93, Nutrientes.NITROGENIO);
-        assertEquals(correcao.getValor(), correcaoFosforo.forneceCorrecaoFosforoDois().getValor(), 0.1);
-        assertEquals(correcao.getDescricao_item(), correcaoFosforo.forneceCorrecaoFosforoDois().getDescricao_item());
-
-        correcaoFosforo = new CorrecaoFosforo(12, FontesFosforo.TERMOFOSTATO_YOORIN, 70, 1260, ecc);
-        correcao = new ForneceCorrecao(34.70, Nutrientes.CALCIO);
-        assertEquals(correcao.getValor(), correcaoFosforo.forneceCorrecaoFosforoDois().getValor(), 0.1);
-        assertEquals(correcao.getDescricao_item(), correcaoFosforo.forneceCorrecaoFosforoDois().getDescricao_item());
-
-        correcaoFosforo = new CorrecaoFosforo(12, FontesFosforo.FOSFATO_ARAD, 70, 1260, ecc);
-        correcao = new ForneceCorrecao(35.15, Nutrientes.CALCIO);
-        assertEquals(correcao.getValor(), correcaoFosforo.forneceCorrecaoFosforoDois().getValor(), 0.1);
-        assertEquals(correcao.getDescricao_item(), correcaoFosforo.forneceCorrecaoFosforoDois().getDescricao_item());
-
-        correcaoFosforo = new CorrecaoFosforo(12, FontesFosforo.FOSFATO_GAFSA, 70, 1260, ecc);
-        correcao = new ForneceCorrecao(40.00, Nutrientes.CALCIO);
-        assertEquals(correcao.getValor(), correcaoFosforo.forneceCorrecaoFosforoDois().getValor(), 0.1);
-        assertEquals(correcao.getDescricao_item(), correcaoFosforo.forneceCorrecaoFosforoDois().getDescricao_item());
-
-        correcaoFosforo = new CorrecaoFosforo(12, FontesFosforo.FOSFATO_DAOUI, 70, 1260, ecc);
-        correcao = new ForneceCorrecao(31.37, Nutrientes.CALCIO);
-        assertEquals(correcao.getValor(), correcaoFosforo.forneceCorrecaoFosforoDois().getValor(), 0.1);
-        assertEquals(correcao.getDescricao_item(), correcaoFosforo.forneceCorrecaoFosforoDois().getDescricao_item());
-
-        correcaoFosforo = new CorrecaoFosforo(12, FontesFosforo.FOSFATO_NAT_PATOS_MINAS, 70, 1260, ecc);
-        correcao = new ForneceCorrecao(26.02, Nutrientes.CALCIO);
-        assertEquals(correcao.getValor(), correcaoFosforo.forneceCorrecaoFosforoDois().getValor(), 0.1);
-        assertEquals(correcao.getDescricao_item(), correcaoFosforo.forneceCorrecaoFosforoDois().getDescricao_item());
-
-        correcaoFosforo = new CorrecaoFosforo(12, FontesFosforo.ESCORIA_THOMAS, 70, 1260, ecc);
-        correcao = new ForneceCorrecao(53.06, Nutrientes.CALCIO);
-        assertEquals(correcao.getValor(), correcaoFosforo.forneceCorrecaoFosforoDois().getValor(), 0.1);
-        assertEquals(correcao.getDescricao_item(), correcaoFosforo.forneceCorrecaoFosforoDois().getDescricao_item());
-
-        correcaoFosforo = new CorrecaoFosforo(12, FontesFosforo.ACIDO_FOSFORICO, 70, 1260, ecc);
-        correcao = new ForneceCorrecao(0.0, null);
-        assertEquals(correcao.getValor(), correcaoFosforo.forneceCorrecaoFosforoDois().getValor(), 0.1);
-        assertEquals(correcao.getDescricao_item(), correcaoFosforo.forneceCorrecaoFosforoDois().getDescricao_item());
-
-        correcaoFosforo = new CorrecaoFosforo(12, FontesFosforo.MULTIFOSFATO_MAGNESIANO, 70, 1260, ecc);
-        correcao = new ForneceCorrecao(22.31, Nutrientes.CALCIO);
-        assertEquals(correcao.getValor(), correcaoFosforo.forneceCorrecaoFosforoDois().getValor(), 0.1);
-        assertEquals(correcao.getDescricao_item(), correcaoFosforo.forneceCorrecaoFosforoDois().getDescricao_item());
-    }
 
     @Test
     public void testeCustoAlqueire() {
